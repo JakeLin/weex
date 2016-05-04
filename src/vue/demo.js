96db51a24dc975a1095bee3fa6ce18010ef14321
Jinjiang <zhaojinjiang@me.com>
2016-05-04 03:23:09 +0100

    * [jsfm] added comments for new format

--- src/vue/demo.js
+++ src/vue/demo.js
@@ -1,14 +1,82 @@
+/**
+ * format comparison
+ *
+ * old:
+ * __h__(tag, data, children, namespace)
+ * new:
+ * __r__(self, children)       -> render
+ * __s__(tag, data, namespace) -> self
+ * __t__(text)                 -> text content
+ * __m__(index)                -> static tree
+ *
+ * the new format MUST be with `stream: true` option from root Vm
+ */
 Vue.component('foo', {
   props: ['a', 'b'],
   data: function () {
     return {list: [1,2]}
   },
+  // // new format
+  // stream: true,
   render: function () {
-    with (this) { return __h__('div', undefined, [(list)&&__renderList__((list), function(k, v,$index){return __h__('div', {class:[a, 'a'],style:{m: '1', n: a},attrs:{"n":a},staticAttrs:{"m":"a","style":"l: 2"}}, [("\n aaa"+__toString__(a)+"bbb"+__toString__(k)+"-"+__toString__(v)+"\n "),_staticTrees[0]], '')})], '')}
+    with (this) {
+      // old format
+      return __h__('div', undefined, [
+        (list) && __renderList__((list), function (k, v,$index) {
+          return __h__('div', {
+            class:[a, 'a'],
+            style:{m: '1', n: a},
+            attrs:{"n":a},
+            staticAttrs:{"m":"a","style":"l: 2"}
+          }, [
+            ("\n aaa"+__toString__(a)+"bbb"+__toString__(k)+"-"+__toString__(v)+"\n "),
+            _staticTrees[0]
+          ], '')
+        })
+      ], '')
+      // // new format
+      // return __r__(
+      //   __s__('div', {
+      //     staticAttrs: {a: 'b'}
+      //   }),
+      //   [
+      //     __r__(
+      //       __s__('text', {
+      //         staticAttrs: {value: 'child text content'}
+      //       })
+      //     ),
+      //   (list) && __renderList__((list), function(k, v, $index) {
+      //     return __r__(
+      //       __s__('div', {
+      //         class:[a, 'a'],
+      //         style:{m: '1', n: a},
+      //         attrs:{"n":a},
+      //         staticAttrs:{"m":"a","style":"l: 2"}
+      //       }),
+      //       [
+      //         __t__("\n aaa"+__toString__(a)+"bbb"+__toString__(k)+"-"+__toString__(v)+"\n "),
+      //         __m__(0)
+      //       ]
+      //     )
+      //   })]
+      // )
+    }
   },
   staticRenderFns: [
     function () {
-      with(this){return __h__('text', {staticAttrs: {value: 'Hello World'}}, [], '')}
+      with(this){
+        // old format
+        return __h__('text', {
+          staticAttrs: {value: 'Hello World'}
+        }, [], '')
+        // // new format
+        // return __r__(
+        //   __s__('text', {
+        //     staticAttrs: {value: 'Hello World'}
+        //   }),
+        //   []
+        // )
+      }
     }
   ]
 })
@@ -21,18 +89,33 @@ return new Vue({
     this.y++
   }},
   style: {y: {fontSize: 64}},
+  // // new format
   // stream: true,
-  // old:
-  // __h__(tag, data, children, namespace)
-  // new:
-  // __r__(self, children)       -> render
-  // __s__(tag, data, namespace) -> self
-  // __c__(children)             -> children
   render: function () {
     with (this) {
+      // old format
+      return __h__(
+        'div', {},
+        [
+          __h__('foo', {attrs:{"a":x,"b":y}}, undefined, ''),
+          __h__('text', {
+            on:{"click":foo},
+            staticClass:"a b",
+            class:[y, 'y'],
+            attrs:{value:(__toString__(y)+"asdfasdfaf")}
+          }, [], '')
+        ], ''
+      )
+      // // new format
       // return __r__(
       //   __s__('div', {}),
       //   [
+      //     __r__(
+      //       __s__('foo', {
+      //         attrs:{"a":x,"b":y}
+      //       }),
+      //       []
+      //     ),
       //     __r__(
       //       __s__('text', {
       //         on:{"click":foo},
@@ -45,19 +128,6 @@ return new Vue({
       //     )
       //   ]
       // )
-      return __h__(
-        'div', {},
-        [
-          // __h__('foo', {attrs:{"a":x,"b":y}}, undefined, ''),
-          // __h__(z, {}, undefined),
-          __h__('text', {
-            on:{"click":foo},
-            staticClass:"a b",
-            class:[y, 'y'],
-            attrs:{value:(__toString__(y)+"asdfasdfaf")}
-          }, [], '')
-        ], ''
-      )
     }
   }
 })
