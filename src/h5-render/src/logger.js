9ea988e346d0788c12edcc63a187a440fb4e21bf
MrRaindrop <tekkahs@gmail.com>
2016-05-24 22:55:35 +0800

    * [html5] add lint for src folder

--- src/h5-render/src/logger.js
+++ src/h5-render/src/logger.js
@@ -3,48 +3,48 @@ var config = require('./config')
 var _initialized = false
 
 var logger = {
-	log: function () {},
-	warn: function () {},
-	error: function () {}
+  log: function () {},
+  warn: function () {},
+  error: function () {}
 }
 
 function hijack() {
-	if (console.log) {
-		logger.log = function () {
-			console.log.apply(
-				console,
-				['[h5-render]'].concat(Array.prototype.slice.call(arguments, 0))
-			)
-		}
-	}
+  if (console.log) {
+    logger.log = function () {
+      console.log.apply(
+        console,
+        ['[h5-render]'].concat(Array.prototype.slice.call(arguments, 0))
+      )
+    }
+  }
 
-	if (console.warn) {
-		logger.warn = function () {
-			console.warn.apply(
-				console,
-				['[h5-render]'].concat(Array.prototype.slice.call(arguments, 0))
-			)
-		}
-	}
+  if (console.warn) {
+    logger.warn = function () {
+      console.warn.apply(
+        console,
+        ['[h5-render]'].concat(Array.prototype.slice.call(arguments, 0))
+      )
+    }
+  }
 
-	if (console.error) {
-		logger.error = function () {
-			console.error.apply(
-				console,
-				['[h5-render]'].concat(Array.prototype.slice.call(arguments, 0))
-			)
-		}
-	}
+  if (console.error) {
+    logger.error = function () {
+      console.error.apply(
+        console,
+        ['[h5-render]'].concat(Array.prototype.slice.call(arguments, 0))
+      )
+    }
+  }
 }
 
 logger.init = function () {
-	if (_initialized) {
-		return
-	}
-	_initialized = true
-	if (config.debug && console) {
-		hijack()
-	}
+  if (_initialized) {
+    return
+  }
+  _initialized = true
+  if (config.debug && console) {
+    hijack()
+  }
 }
 
 module.exports = logger
