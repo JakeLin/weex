9b0daa17ec670a213ee4297d0309f7f278de07de
MrRaindrop <tekkahs@gmail.com>
2016-05-25 16:17:27 +0800

    * [html5] add warnings for scrollable roots

--- src/h5-render/src/components/root.js
+++ src/h5-render/src/components/root.js
@@ -3,8 +3,9 @@
 var ComponentManager = require('../componentManager')
 var Component = require('./component')
 var utils = require('../utils')
+var logger = require('../logger')
 
-var rootCandidates = ['list', 'scroller']
+var rootCandidates = ['list', 'scroller', 'vlist']
 
 function RootComponent(data, nodeType) {
   var id = data.rootId + '-root'
@@ -17,17 +18,36 @@ function RootComponent(data, nodeType) {
 
   // In some situation the root component should be implemented as
   // its own type, otherwise it has to be a div component as a root.
-  if (!nodeType
-      || rootCandidates.indexOf(nodeType) === -1
-      || downgrades[nodeType]) {
+  if (!nodeType) {
+    nodeType = 'div'
+  } else if (rootCandidates.indexOf(nodeType) === -1) {
+    logger.warn('the root component type \'' + nodeType + '\' is not one of '
+      + 'the types in [' + rootCandidates + '] list. It is auto downgraded '
+      + 'to \'div\'.')
+    nodeType = 'div'
+  } else if (downgrades[nodeType]) {
+    logger.warn('Thanks to the downgrade flags for ['
+      + Object.keys(downgrades)
+      + '], the root component type \'' + nodeType
+      + '\' is auto downgraded to \'div\'.')
     nodeType = 'div'
   } else {
     // If the root component is not a embed element in a webpage, then
     // the html and body height should be fixed to the max height
     // of viewport.
     if (!componentManager.weexInstance.embed) {
-      this.fixRootHeight()
+      window.addEventListener('renderend', function () {
+        this.detectRootHeight()
+      }.bind(this))
     }
+    logger.warn('the root component type \'' + nodeType + '\' may have '
+      + 'some performance issue on some of the android devices when there '
+      + 'is a huge amount of dom elements. Try to add downgrade '
+      + 'flags by adding param \'downgrade_' + nodeType + '=true\' in the '
+      + 'url or setting downgrade config to a array contains \'' + nodeType
+      + '\' in the \'weex.init\' function. This will downgrade the root \''
+      + nodeType + '\' to a \'div\', and may elevate the level of '
+      + 'performance, although it has some other issues.')
     !this.data.style.height && (this.data.style.height = '100%')
   }
 
@@ -39,15 +59,21 @@ function RootComponent(data, nodeType) {
 
 RootComponent.prototype = Object.create(Component.prototype)
 
-RootComponent.prototype.fixRootHeight = function () {
-  var rootContainer = document.querySelector(
-      '#' + this.getComponentManager().weexInstance.rootId
-    ) || document.body
-  var html = document.documentElement
-  var body = document.body
-  !html.style.height && (html.style.height = '100%')
-  !body.style.height && (body.style.height = '100%')
-  !rootContainer.style.height && (rootContainer.style.height = '100%')
+RootComponent.prototype.detectRootHeight = function () {
+  var rootQuery = '#' + this.getComponentManager().weexInstance.rootId
+  var rootContainer = document.querySelector(rootQuery) || document.body
+  var height = rootContainer.getBoundingClientRect().height
+  if (height > window.innerHeight) {
+    logger.warn([
+      'for scrollable root like \'list\' and \'scroller\', the height of ',
+      'the root container musted be a user-specified value. Otherwise ',
+      'the scrollable element may not be able to work correctly. ',
+      'Current height of the root element \'' + rootQuery + '\' is ',
+      height + 'px, and mostly its height should be less than the ',
+      'viewport\'s height ' + window.innerHeight + 'px. Please ',
+      'make sure the height is correct.'
+      ].join(''))
+  }
 }
 
 module.exports = RootComponent
