7d0695a4104f4b3897cbee1c28feb3bc6e0b4a5f
MrRaindrop <tekkahs@gmail.com>
2016-06-01 00:44:45 +0800

    * [html5] fix <a> component

--- src/h5-render/src/components/a.js
+++ src/h5-render/src/components/a.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var logger = require('../logger')
 var Component = require('./component')
 
 // attrs:
@@ -14,10 +15,22 @@ A.prototype.create = function () {
   var node = document.createElement('a')
   node.classList.add('weex-container')
   this.node = node
-  this.style.alignItems.call(this, 'center')
-  this.style.justifyContent.call(this, 'center')
+  node.addEventListener('click', function (e) {
+    location.href = this.href
+  })
   this.node.style.textDecoration = 'none'
   return node
+}
+
+A.prototype.attr = {
+	href: function (val) {
+		val += ''
+		if (!val) {
+			return logger.warn('href of <a> should not be a null value.')
+		}
+		this.href = val
+		this.node.setAttribute('href', val)
+	}
 }
 
 module.exports = A
